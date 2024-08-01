c73972c13acf25a132b0e4447dc6231cf2962e8a
fabianacosta97 <73755717+fabianacosta97@users.noreply.github.com>
2024-08-01 20:47:48 -0300

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,10 +1,121 @@
-const opciones = [
-  { id: 'opcion1', nombre: 'Opción 1', descripcion: 'Descripción de la opción 1 \n asdad.', imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', grupo: 1, visible: true },
-  { id: 'opcion2', nombre: 'Opción 2', descripcion: 'Descripción de la opción 2.', imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', grupo: 1, visible: true },
-  { id: 'opcion3', nombre: 'Opción 3', descripcion: 'Descripción de la opción 3.', imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', grupo: 2, visible: true }
-];
-
-
 const descripcionInfo = `Programación para entregar hasta el 06 de Agosto
 6 menús $27.000,00 - (sería cada uno $4.500,00) - envío gratis.
 1 menú o más cada uno $5.000,00 - envió a consultar.`;
+
+const opciones = [
+  { 
+    id: 'opcion1',
+    grupo: 1, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'OMELETT CON VERDURA', 
+    descripcion: 'OMELETT: de 3 huevo, con sofrito de verduras y 4 quesos, acompañado de tortilla de papas.', 
+  },
+  { 
+    id: 'opcion2',
+    grupo: 1, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'OMELETT CON PAPA', 
+    descripcion: 'OMELETT: de 3 huevos, con sofrito de verduras y 4 quesos, acompañado de \ntortilla de verduras. (zapallitos, cebollas, zanahorias, morrón).', 
+  },
+  { 
+    id: 'opcion3',
+    grupo: 2, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'OMELETT CON PAPA', 
+    descripcion: 'OMELETT: de 3 huevos, con sofrito de verduras y 4 quesos, acompañado de \ntortilla de verduras. (zapallitos, cebollas, zanahorias, morrón).', 
+  },
+  { 
+    id: 'opcion4',
+    grupo: 2, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'ESCALOPE DE TERNERA', 
+    descripcion: 'ESCALOPE DE TERNERA: carne de ternera, macerada y recubierta por masa, acompañada de papas al natural, con lluvia de perejil.', 
+  },
+  { 
+    id: 'opcion5',
+    grupo: 3, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'ESCALOPE DE BERENJENA', 
+    descripcion: 'ESCALOPE DE BERENJENA: berenjenas macerada y recubierta en masa, acompañado de verduras grilladas.', 
+  },
+    { 
+    id: 'opcion6',
+    grupo: 3, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'CANELONES', 
+    descripcion: 'CANELONES: con carne de ternera, con salsa bolognesa. (carne de ternera picada. Tomate, cebolla, ajo y vino tinto).', 
+  },
+    { 
+    id: 'opcion7',
+    grupo: 4, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'CANELONES DE POLLO', 
+    descripcion: 'CANELONES DE POLLO: con carne de pollo y verduras, acompañado de salsa bechamel.', 
+  },
+    { 
+    id: 'opcion8',
+    grupo: 4, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'OMELETT CON PAPA', 
+    descripcion: 'OMELETT: de 3 huevos, con sofrito de verduras y 4 quesos, acompañado de \ntortilla de verduras. (zapallitos, cebollas, zanahorias, morrón).', 
+  },
+    { 
+    id: 'opcion9',
+    grupo: 5, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'RISOTTO CON ALBÓNDIGAS', 
+    descripcion: 'RISOTTO: arroz doble carolina, sellado en aceite de oliva e hidratado con caldo de verduras y 4 quesos, \ncon albóndigas (carne de ternera picada, con cebolla y morrón y salsa de tomate).', 
+  },
+    { 
+    id: 'opcion10',
+    grupo: 5, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'RISOTTO CON POLLO', 
+    descripcion: 'RISOTTO: arroz doble carolina, sellado en aceite de oliva, e hidratado con caldo de verdura y salsa de puerro, con pata de pollo grillado.', 
+  },
+    { 
+    id: 'opcion11',
+    grupo: 6, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'ROULET DE POLLO', 
+    descripcion: 'ROULET DE POLLO: Pechuga de pollo enrollada, rellena de con jamón, queso, zanahoria, morrón y huevo duro; \n acompañado de ensalada con verduras de estación.', 
+  },
+    { 
+    id: 'opcion12',
+    grupo: 6, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'CUARTO DE POLLO', 
+    descripcion: 'CUARTO DE POLLO: de 3 huevos, con sofrito de verduras y 4 quesos, acompañado de \ntortilla de verduras. (zapallitos, cebollas, zanahorias, morrón).', 
+  },
+    { 
+    id: 'opcion13',
+    grupo: 7, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'ZAPALLITOS RELLENOS', 
+    descripcion: 'ZAPALLITOS RELLENOS: relleno con carne de ternera molida, cebolla, moron, y queso, con salsa bolognesa. (carne de ternera picada. Tomate, cebolla, ajo y vino tinto).', 
+  },
+    { 
+    id: 'opcion14',
+    grupo: 7, 
+    visible: true,
+    imgSrc: 'https://i.imgur.com/cfwJR64.jpeg', 
+    nombre: 'PASTAS FRESCAS', 
+    descripcion: 'PASTAS FRESCAS: Pastas caseras tallarín, en sémola y huevo; con salsa bolognesa (carne picada, cebolla, tomate, ajo y vino tinto).', 
+  },
+];
+
+
+
